c7317e6733356fce88c29bf569defe924e9394cc
Felipe Gonçalves <47393970+lipiw@users.noreply.github.com>
2023-09-26 18:45:50 -0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -23,36 +23,10 @@ import "slick-carousel/slick/slick-theme.css";
 export function Home() {
   const settings = {
     infinite: true,
-    speed: 400,
+    speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //       initialSlide: 2,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    // ],
   };
 
   return (
