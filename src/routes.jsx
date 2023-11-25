0ab3bf565f9c8190380d8b93d6e962201922f4c4
Felipe Gonçalves <47393970+lipiw@users.noreply.github.com>
2023-11-25 11:02:04 -0300

    Update routes.jsx

---
+++ src/routes.jsx
@@ -0,0 +1,24 @@
+import { Home } from "@/pages";
+import {
+  HomeIcon,
+  UserCircleIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/solid";
+
+export const routes = [
+  {
+    icon: HomeIcon,
+    name: "home",
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    icon: DocumentTextIcon,
+    name: "Docs",
+    href: "https://www.material-tailwind.com/docs/react/installation",
+    target: "_blank",
+    element: "",
+  },
+];
+
+export default routes;
